8d59fa2253692d0b03e3ae78589ae3673cbd4228
Shivam Singh <103557291+SiNGH132k4@users.noreply.github.com>
2025-09-07 17:40:15 +0530

    Update gemini.js

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -7,7 +7,7 @@ import {
     HarmBlockThreshold,
 } from "@google/generative-ai";
 
-const apiKey = API_KEY;
+const apiKey = import.meta.env.VITE_GEMINI_API_KEY;
 const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({
